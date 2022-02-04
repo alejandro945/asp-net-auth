d6e04ef9bb64f1b390b39e2081096ae5005ecb9a
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2022-02-04 17:19:02 -0500

    Startin the login

--- Models/User.cs
+++ Models/User.cs
@@ -2,12 +2,17 @@
 {
     public class User
     {
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
         public DateTime Birth { get; set; }
         public int ID { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
 
 
      
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using asp_net_auth.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp_net_auth.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
 
+        }
+
+        private readonly asp_net_auth.Data.asp_net_authContext _context;
+
+        public IndexModel(asp_net_auth.Data.asp_net_authContext context)
+        {
+            _context = context;
+        }
+
+        private bool UserExists(string username, string password)
+        {
+
+            return _context.User.Any(e => e.Username.Equals(username) && e.Password.Equals(password));
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+        
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(User).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(User.Username, User.Password))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            Console.Write("Varela sapa");
+
+            return RedirectToPage("./Users");
         }
     }
 }
